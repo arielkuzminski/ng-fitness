0e62cdc01d071860dc6ea99be15d5dd4f5c5dffb
Ariel <ariel.kuzminski@gmail.com>
2018-04-16 17:58:54 +0200

    login and signup template

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form: FormControl) {
+    console.log(form);
+  }
+
+}
