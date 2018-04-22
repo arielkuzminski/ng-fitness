1b98540f70c0158d04a68d49cea2f9168eccf459
Ariel <ariel.kuzminski@gmail.com>
2018-04-22 14:36:59 +0200

    Added training table and pagination

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,13 +1,19 @@
+import { Subject } from 'rxjs/Subject';
+
 import { Exercise } from './exercise.model';
 
 export class TrainingService {
+  exerciseChanged = new Subject<Exercise>();
+
   private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+    { id: 'crunches', name: 'Crunches', duration: 3, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 18, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 12, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 6, calories: 8 },
+    { id: 'test', name: 'test', duration: 10, calories: 10 }
   ];
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
   getAvailableExercises(): Exercise[] {
     return this.availableExercises.slice();
@@ -15,5 +21,36 @@ export class TrainingService {
 
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+    this.exerciseChanged.next({ ...this.runningExercise });
+  }
+
+  completeExercise() {
+    this.exercises.push({
+      ...this.runningExercise,
+      date: new Date(),
+      state: 'completed'
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100),
+      calories: this.runningExercise.calories * (progress / 100),
+      date: new Date(),
+      state: 'cancelled'
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
+  }
+
+  getCompletedOrCancelledExercises() {
+    return this.exercises.slice();
   }
 }
